a26b8cc2d7922bf7f99216dd43e2a4b8644286f2
Zach <z.cuneo03@gmail.com>
2022-07-22 09:10:15 -0700

    Done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let isModalOpen = false;
 let contrastToggle = false;
 
+const scaleFactor = 1 / 20;
+
 // Email Contact Form
 
 function contact() {
@@ -26,7 +28,7 @@ function contact() {
   })  
 };
 
-// Toggle Contact Me Modal
+// Toggle Mail Icon Modal
 
 function toggleModal() {
   if (isModalOpen) {
@@ -46,4 +48,19 @@ function toggleContrast() {
   } else {
     document.body.classList.remove("dark-theme");
   }
-}
+};
+
+// Move Background
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+  }
+};
